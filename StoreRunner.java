00280f0907d5cb70e0ba10db9efddb4be1ae1691
AlejandroC70531 <achavezjr0017@mymail.lausd.net>
2024-09-30 19:48:30 -0700

    Update StoreRunner.java

--- StoreRunner.java
+++ StoreRunner.java
@@ -8,7 +8,7 @@ public class StoreRunner {
  */
  ItalianFood i = new ItalianFood();
  System.out.println(i.toString());
- ItalianFood t = new ItalianFood("t", 600, 4.99);
+ ItalianFood t = new ItalianFood("t", 4.99);
  System.out.println(t.toString());
  ItalianFood spaghetti = new ItalianFood("Spaghetti", 1100, 21.99);
  System.out.println(spaghetti.toString());
@@ -24,7 +24,7 @@ public class StoreRunner {
  System.out.println(p.toString());
  Pasta arrabiata = new Pasta("Penne alla Arrabiata", 1400, 22.99, "arrabiata", "penne", true);
  System.out.println(arrabiata.toString());
- Pasta puttanesca = new Pasta("Spaghetti alla Puttanesca", 850, 18.99, "puttanesca", "spaghetti", false);
+ Pasta puttanesca = new Pasta("Spaghetti alla Puttanesca", 850, 18.99, "puttanesca", "spaghetti");
  System.out.println(puttanesca.toString());
 
  puttanesca.setPastaType("rigatoni");
@@ -34,7 +34,7 @@ public class StoreRunner {
  */
  Pizza z = new Pizza();
  System.out.println(p.toString());
- Pizza margherita = new Pizza("Pizza Margherita", 1800, 29.99, "medium", "nothing", 8);
+ Pizza margherita = new Pizza("Pizza Margherita", 1800, 29.99, "medium");
  System.out.println(margherita.toString());
  Pizza siciliana = new Pizza("Pizza Siciliana", 2260, 33.99, "large", "onions, anchovies", 10);
  System.out.println(siciliana.toString());
@@ -48,7 +48,7 @@ public class StoreRunner {
   System.out.println(g.toString());
   Gelato pistachio = new Gelato("Pistachio Gelato", 650, 8.99, "pistachio", true);
   System.out.println(pistachio.toString());
-  Gelato stracciatella = new Gelato("Stracciatella Gelato", 800, 11.99, "stracciatella", false);
+  Gelato stracciatella = new Gelato("Stracciatella Gelato", 800, 11.99, "stracciatella");
   System.out.println(stracciatella.toString());
 
  pistachio.setIsInCone(false);
